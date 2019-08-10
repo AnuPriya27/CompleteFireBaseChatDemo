e7b48db54b960c433cc49fc8f2ee087820921da9
imsaurabhm <imsaurabhmahajan@gmail.com>
2019-08-10 13:59:41 +0530

    New Changes

--- CompleteFirebaseChatApp/AppDelegate.swift
+++ CompleteFirebaseChatApp/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     override init() {
         FirebaseApp.configure()
+        print("Hello")
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
